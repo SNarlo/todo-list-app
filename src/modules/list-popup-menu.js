45a53abc3745447045486c3d44bb1cec11275026
Sebastian <sebastian.narloch82@gmail.com>
2021-02-28 16:04:44 +1000

    Working on adding a list item to the list bar

--- src/modules/list-popup-menu.js
+++ src/modules/list-popup-menu.js
@@ -1,9 +1,12 @@
+import createListItem from './list-item';
+
 /**
  * A pop up window for when creating a todo item
  */
 const PopupMenu = (() => {
     
     const listPopup = document.querySelector('.list-popup');
+    const popupInputBox = document.querySelector('.list-name');
 
     const openPopupWindow = () => {
         let addButton = document.querySelector('.add-list-item');
@@ -20,10 +23,32 @@ const PopupMenu = (() => {
         })
     }
 
+    const addListItem = () => {
+        let addButton = document.querySelector('.add-list');
+
+        if ((popupInputBox.value != null)) { // need to fix this !!
+            console.log(popupInputBox.value);
+            let listName = popupInputBox.textContent;
+            addButton.addEventListener('click', createListItem.createListContainer(listName));
+        }
+        
+        if (!(popupInputBox && popupInputBox.value)) {
+            addButton.addEventListener('click', () =>{
+                
+                popupInputBox.className = 'list-name-invalid';
+                setInterval(() => {popupInputBox.className = 'list-name'}, 700); // change class name back
+            });
+        }
+        
+        
+        
+    }
+    
+
     return {
         openPopupWindow,
         closePopupWindow,
-
+        addListItem,
     }
 
 })();
